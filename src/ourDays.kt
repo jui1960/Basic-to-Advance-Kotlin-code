8eb30d60f223aee6ef7c2dec9d2f54ce673a5277
jui1960 <juifull196044@gmail.com>
2026-06-28 20:18:33 +0600

    our days

---
+++ src/ourDays.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    val quote = "LIFE IS NOT A PROBLEM TO BE SOLVED"
+
+    println(quote.substring(0, n))
+
+}
